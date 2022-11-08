7e9775a334ea6132457f99305cc15a813adab020
jsc-repo <108231974+jsc-repo@users.noreply.github.com>
2022-11-08 10:35:58 -0500

    Configure API invokeURL

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
-// Add your API invoke URL below, configured in Module 3. 
+// Add your API invoke URL below, configured in Module 3.
 
 module.exports = {
-    api: {
-        invokeUrl: '' // e.g. https://rc2345678345.execute-api.us-west-2.amazonaws.com/prod,
-    }
-}
+  api: {
+    invokeUrl: "https://p20q318f6j.execute-api.us-east-1.amazonaws.com/prod", // e.g. https://rc2345678345.execute-api.us-west-2.amazonaws.com/prod,
+  },
+};
